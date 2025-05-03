6a740e343dd885b6676be70340509475e9118517
phevabb <phevab1@gmail.com>
2025-05-03 03:37:49 +0000

    updates

--- core/forms.py
+++ core/forms.py
@@ -10,12 +10,12 @@ class StudentPerformanceForm(forms.Form):
 
     model_choice = forms.ChoiceField(label="Choose Model", choices=MODEL_CHOICES)
 
-    hours_studied = forms.FloatField(label="Hours Studied", min_value=0)
-    previous_scores = forms.FloatField(label="Previous Scores", min_value=0)
+    hours_studied = forms.FloatField(label="Hours Studied", min_value=0, max_value=24)
+    previous_scores = forms.FloatField(label="Previous Scores", min_value=0, max_value=100)
     extracurricular_activities = forms.ChoiceField(
         label="Extracurricular Activities",
         choices=[(1, 'Yes'), (0, 'No')],
         widget=forms.Select()
     )
-    sleep_hours = forms.FloatField(label="Sleep Hours", min_value=0)
-    sample_question_papers_practiced = forms.IntegerField(label="Sample Question Papers Practiced", min_value=0)
+    sleep_hours = forms.FloatField(label="Sleep Hours", min_value=0, max_value=12)
+    sample_question_papers_practiced = forms.IntegerField(label="Sample Question Papers Practiced", min_value=0, max_value=20)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import os
 import joblib
 from django.conf import settings
@@ -20,7 +21,7 @@ def predict_performance(request):
         form = StudentPerformanceForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            print(f"dataaaaaaaaaaaaa: {data}")
+
             input_data = [[
                 data['hours_studied'],
                 data['previous_scores'],
@@ -31,18 +32,35 @@ def predict_performance(request):
 
             selected_model = data['model_choice']
             if selected_model == 'linear':
-                prediction = model_lin.predict(input_data)[0]
+                student_data = pd.DataFrame(input_data, columns=['Hours_Studied', 'Previous_Scores',
+                                                                 'Extracurricular_Activities',
+                                                                    'Sleep_Hours', 'Sample_Question_Papers_Practiced'])
+                prediction = model_lin.predict(student_data)[0]
                 return render(request, 'performance_index.html', {'p_index':prediction,'model_used': 'linear regression'})
             elif selected_model == 'decision_tree':
-                prediction = model_tree.predict(input_data)[0]
+                student_data = pd.DataFrame(input_data,
+                                            columns=['Hours_Studied', 'Previous_Scores', 'Extracurricular_Activities',
+                                                     'Sleep_Hours', 'Sample_Question_Papers_Practiced'])
+
+                prediction = model_tree.predict(student_data)[0]
                 return render(request, 'performance_index.html', {'p_index': prediction,'model_used': 'decision tree'})
 
             elif selected_model == 'random_forest':
-                prediction = model_forest.predict(input_data)[0]
+                student_data = pd.DataFrame(input_data,
+                                            columns=['Hours_Studied', 'Previous_Scores', 'Extracurricular_Activities',
+                                                     'Sleep_Hours', 'Sample_Question_Papers_Practiced'])
+
+                prediction = model_forest.predict(student_data)[0]
                 return render(request, 'performance_index.html', {'p_index': prediction,'model_used': 'random forest'})
 
             elif selected_model == 'svm':
-                prediction = model_svr.predict(input_data)[0]
+                student_data = pd.DataFrame(input_data,
+                                            columns=['Hours_Studied', 'Previous_Scores', 'Extracurricular_Activities',
+                                                     'Sleep_Hours', 'Sample_Question_Papers_Practiced'])
+
+                prediction = model_svr.predict(student_data)[0]
+
+
                 return render(request, 'performance_index.html', {'p_index': prediction,'model_used': 'support vector machine'})
 
             else:
